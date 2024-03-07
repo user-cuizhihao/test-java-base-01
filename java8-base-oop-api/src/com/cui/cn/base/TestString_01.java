b63afedd1cc8ffbe2b7389e9c34a7a4ddb908b25
cuizhihao <18336303078@163.com>
2024-03-07 18:23:55 +0800

    提交所有2

--- java8-base-oop-api/src/com/cui/cn/base/TestString_01.java
+++ java8-base-oop-api/src/com/cui/cn/base/TestString_01.java
@@ -1,4 +1,8 @@
 package com.cui.cn.base;
 
 public class TestString_01 {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ java8-base-oop-api/src/com/cui/cn/base/TestString_02.java
@@ -0,0 +1,5 @@
+package com.cui.cn.base;
+
+public class TestString_02 {
+
+}
