392a0f3318759b8a07ae2a46652470de55cc0893
cuizhihao <18336303078@163.com>
2024-03-11 16:25:18 +0800

    提交git测试文件

---
+++ java8-base-oop-api/src/com/cui/cn/base/Test001.java
@@ -0,0 +1,13 @@
+package com.cui.cn.base;
+
+/**
+ * @author 86183 - cuizhihao
+ * @create 2024-03-10-3:58
+ */
+public class Test001 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- java8-base-oop-api/src/com/cui/cn/base/TestString.java
+++ java8-base-oop-api/src/com/cui/cn/base/TestString.java
@@ -6,6 +6,8 @@ public class TestString {
 
         TestString t1 = new TestString();
         int i = t1.method001();
+        int i1 = t1.method002();
+        System.out.println(i1);
         System.out.println(i);
     }
 
@@ -14,7 +16,15 @@ public class TestString {
         int a = (++i) + (++i);
         System.out.println(i);
         return a;
-
     }
+
+    public int method002(){
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        return 0;
+    }
+
 }
 
