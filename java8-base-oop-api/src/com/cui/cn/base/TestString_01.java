7374fd5ae6fa1fe5c460dcba801d2998e571f241
cuizhihao <18336303078@163.com>
2024-03-17 05:37:53 +0800

    参数列表使用final

--- java8-base-oop-api/src/com/cui/cn/base/TestString_01.java
+++ java8-base-oop-api/src/com/cui/cn/base/TestString_01.java
@@ -5,4 +5,13 @@ public class TestString_01 {
     public static void main(String[] args) {
 
     }
+
+    public int method01(final int a,final int [] arr){
+
+//        a = 100; //错误
+//        arr = new int[]{}; // 错误
+
+        return 100;
+    }
+
 }
