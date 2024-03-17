02a018d1a50d5d7b4ebbece17708e1d03c7f7a03
cuizhihao <18336303078@163.com>
2024-03-18 04:59:44 +0800

    测试hashCode 和 equals 方法

--- java8-base-oop-api/src/com/cui/cn/oop/StudentBean.java
+++ java8-base-oop-api/src/com/cui/cn/oop/StudentBean.java
@@ -1,6 +1,65 @@
-package com.cui.cn.oop;/**
+package com.cui.cn.oop;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+/**
  * @author 86183 - cuizhihao
  * @create 2024-03-18-2:25
  */
-    public class StudentBean {
+public class StudentBean {
+
+    private Integer uuid;
+
+    private String name;
+
+    private Date dateTime;
+
+    private BigDecimal money;
+
+    public Integer getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Integer uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentBean that = (StudentBean) o;
+        return Objects.equals(uuid, that.uuid) && Objects.equals(name, that.name) && Objects.equals(dateTime, that.dateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, name, dateTime);
+    }
 }
--- java8-base-oop-api/src/com/cui/cn/oop/TestBean.java
+++ java8-base-oop-api/src/com/cui/cn/oop/TestBean.java
@@ -1,6 +1,28 @@
-package com.cui.cn.oop;/**
+package com.cui.cn.oop;
+
+/**
  * @author 86183 - cuizhihao
  * @create 2024-03-18-4:12
  */
-    public class TestBbean {
+public class TestBean {
+
+    public static void main(String[] args) {
+
+        StudentBean sb1 = new StudentBean();
+        sb1.setUuid(1001);
+        StudentBean sb2 = new StudentBean();
+        sb2.setUuid(100);
+        System.out.println(sb1);
+        System.out.println(sb2);
+
+        System.out.println(sb1.hashCode());
+        System.out.println(sb2.hashCode());
+
+        System.out.println(sb1.equals(sb2));
+    }
+
+    public void method01(){
+
+    }
+
 }
