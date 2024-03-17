c84aeea13cc65319465279aaa311813752007fbd
cuizhihao <18336303078@163.com>
2024-03-18 04:59:04 +0800

    测试hashCode 和 equals 方法

---
+++ java8-base-oop-api/src/com/cui/cn/oop/StudentBean.java
@@ -0,0 +1,6 @@
+package com.cui.cn.oop;/**
+ * @author 86183 - cuizhihao
+ * @create 2024-03-18-2:25
+ */
+    public class StudentBean {
+}
---
+++ java8-base-oop-api/src/com/cui/cn/oop/TestBean.java
@@ -0,0 +1,6 @@
+package com.cui.cn.oop;/**
+ * @author 86183 - cuizhihao
+ * @create 2024-03-18-4:12
+ */
+    public class TestBbean {
+}
