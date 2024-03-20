e59c6754cbda2f71ef515b9228bc5592037602bc
cuizhihao <18336303078@163.com>
2024-03-20 19:50:10 +0800

    时间

---
+++ java8-base-oop-api/src/com/cui/cn/api/DateTest.java
@@ -0,0 +1,24 @@
+package com.cui.cn.api;
+
+import java.util.Date;
+
+/**
+ * @author 86183 - cuizhihao
+ * @create 2024-03-20-19:46
+ */
+public class DateTest {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public void dateMethod01(){
+        Date date = new Date();
+        System.out.println(date);
+
+
+    }
+
+}
