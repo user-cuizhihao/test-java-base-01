730b072f542b531c565a1c06f85b21c8b4a8ac13
cuizhihao <18336303078@163.com>
2024-04-07 13:28:37 +0800

    构建springboot项目

---
+++ spring-boot-jdk8/src/main/java/com/cui/cn/SpringBootJdk8Application.java
@@ -0,0 +1,13 @@
+package com.cui.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdk8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdk8Application.class, args);
+    }
+
+}
---
+++ spring-boot-jdk8/src/test/java/com/cui/cn/SpringBootJdk8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cui.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdk8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
